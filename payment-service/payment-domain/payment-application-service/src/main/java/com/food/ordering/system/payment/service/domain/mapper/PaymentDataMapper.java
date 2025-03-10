f1a220904ab85b8826253cfa09474e51170fac92
Ramiro F. Sosa <ramiro.sosa@gmail.com>
2025-03-10 17:46:24 -0300

    Payment Service - Application Service domain module: Adding Mapper, Config and Ports

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/mapper/PaymentDataMapper.java
@@ -0,0 +1,10 @@
+package com.food.ordering.system.payment.service.domain.mapper;
+
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PaymentDataMapper {
+
+}
---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/message/publisher/PaymentCancelledMessagePublisher.java
@@ -0,0 +1,4 @@
+package com.food.ordering.system.payment.service.domain.ports.output.message.publisher;
+
+public class PaymentCancelledMessagePublisher {
+}
