f3f7c0ccb3cf9418fdefc4af0192ac1741402937
Ramiro F. Sosa <ramiro.sosa@gmail.com>
2025-03-10 17:50:38 -0300

    Payment Service - Application Service domain module: Adding Mapper, Config and Ports - part 2

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/message/publisher/PaymentCancelledMessagePublisher.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/message/publisher/PaymentCancelledMessagePublisher.java
@@ -1,4 +1,7 @@
 package com.food.ordering.system.payment.service.domain.ports.output.message.publisher;
 
-public class PaymentCancelledMessagePublisher {
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.payment.service.domain.event.PaymentCancelledEvent;
+
+public interface PaymentCancelledMessagePublisher extends DomainEventPublisher<PaymentCancelledEvent> {
 }
--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/message/publisher/PaymentCompletedMessagePublisher.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/message/publisher/PaymentCompletedMessagePublisher.java
@@ -1,4 +1,7 @@
 package com.food.ordering.system.payment.service.domain.ports.output.message.publisher;
 
-public interface PaymentCompletedMessagePublisher {
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.payment.service.domain.event.PaymentCompletedEvent;
+
+public interface PaymentCompletedMessagePublisher extends DomainEventPublisher<PaymentCompletedEvent> {
 }
